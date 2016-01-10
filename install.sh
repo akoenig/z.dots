dfb0ce6f979cea0e9c2d127db7c3cbcc30209da8
André König <andre.koenig@posteo.de>
2016-01-10 23:13:05 +0100

    [TASK] Remove log message

--- install.sh
+++ install.sh
@@ -8,8 +8,6 @@
 
 mkdir -p $HOME/.apps
 
-dots_output_info "z is not installed. Installing it ..."
-
 git clone https://github.com/rupa/z.git $HOME/.apps/z
 
 dots_profile_add "source $HOME/.apps/z/z.sh"
